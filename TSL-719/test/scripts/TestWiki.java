c660da8e730f5b8102e89d74fb7755e04ab0e97b
Neeraj Deochake <neerajdeochake12@gmail.com>
2018-08-16 17:52:42 +0530

    Add files via upload

--- TSL-719/test/scripts/TestWiki.java
+++ TSL-719/test/scripts/TestWiki.java
@@ -1,9 +1,11 @@
 package scripts;
 
 import java.io.File;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
@@ -16,7 +18,7 @@ public class TestWiki {
 		FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
 		FirefoxProfile firefoxProfile = new FirefoxProfile();       
 		WebDriver driver = new FirefoxDriver(ffBinary,firefoxProfile);
-		driver.get("https://www.wikipedia.org");
+/*		driver.get("https://www.wikipedia.org");
 		driver.findElement(By.id("js-link-box-en")).click();
 		driver.findElement(By.name("search")).sendKeys("Selenium Software");
 		driver.findElement(By.name("go")).click();
@@ -25,6 +27,18 @@ public class TestWiki {
 		System.out.println("Title: " +title);
 		String str2=driver.findElement(By.id("firstHeading")).getText();
 		System.out.println("Heading: " +str2);
+		*/
+		driver.get("https://www.agiletestingalliance.org/");
+
+		List<WebElement> allElements1 = driver
+			    .findElement(By.xpath("//div[@class='soc-icons']"))
+			    .findElements(By.tagName("a"));
+		
+		for (WebElement we:allElements1) {
+			System.out.println(we.getAttribute("href"));
+		}
+		
+		
 	}
 
 }
